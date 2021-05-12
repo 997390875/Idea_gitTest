543bdf382d5f1c3682eb1315a117d2e6ea4a690a
Zheng_ZhiQi <83944336+997390875@users.noreply.github.com>
2021-05-12 11:39:10 +0800

    Update DemoTest.java

--- Idea_gitTest/src/com/gec/test/DemoTest.java
+++ Idea_gitTest/src/com/gec/test/DemoTest.java
@@ -3,5 +3,6 @@ package com.gec.test;
 public class DemoTest {
     public static void main(String[] args) {
         System.out.println("aaaaaaaaaaaa");
+         System.out.println("aaaaaaaaaaaa");
     }
 }
